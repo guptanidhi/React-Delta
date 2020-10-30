9a6c30353b941f9d865c446426283ae467dc5cbe
vinodjadav <vinodjadav07@gmail.com>
2020-10-30 12:46:28 +0530

    updated code navbar

--- src/Component/Navbar/index.js
+++ src/Component/Navbar/index.js
@@ -1,36 +1,41 @@
 import React from 'react';
-import {Navbar, Nav} from 'react-bootstrap';
+import {Navbar, Nav, Button, Image} from 'react-bootstrap';
 import style from './style.module.scss'
+import Logo from '../../img/logo.png'
 
 const menuJson = [{
     id : 1,
-    name: 'Home'
+    name: 'Open Datasets'
 },{
     id : 2,
-    name: 'Platforms'
+    name: 'Statistical Rigor'
 },{
     id : 3,
-    name: 'Services'
+    name: 'Pricing'
 },{
     id : 4,
-    name: 'Solutions'
-},{
-    id : 5,
-    name: 'Contacts'
+    name: 'Consulting'
 }]
 
 function NavbarCustom(props) {
     return <Navbar expand="lg" className={style.darkBg}>
-            <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+            <Navbar.Brand href="#home">
+              <Image src={Logo} />  
+            </Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end"> 
-            <Nav>
+            <Navbar.Collapse id="basic-navbar-nav" className={style.menuWrapper}> 
+            <Nav className={`${style.menulist} mr-auto ml-auto`}>
                 {menuJson.map((menu)=>{
                     return (
                         <Nav.Link className={style.menu} id={menu.id}>{menu.name}</Nav.Link>
                     )
                 })}
-                
+            </Nav>
+            <Nav className={style.menulist}>
+               <Button variant="outline-light">Create Free Account</Button>
+                <Nav.Link href="#memes" className={style.menu}>
+                    Login
+                </Nav.Link>
             </Nav>
             </Navbar.Collapse>
         </Navbar>
