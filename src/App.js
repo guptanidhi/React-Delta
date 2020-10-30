c8bb564b5c8e464910bd7dc5a70d70676d65dba9
Nidhi Gupta <nidhi.gupta2484@gmail.com>
2020-10-30 14:11:59 +0530

    Nav updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
 import {Container, Row, Col, Carousel} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarCustom from './Component/Navbar'
-import SearchSection from './Component/SearchSection'
-import styles from './styles.module.scss'
-import topCategories from './topCategories'
+import NavbarCustom from './Component/Navbar';
+import SearchSection from './Component/SearchSection';
+import styles from './styles.module.scss';
+import topCategories from './topCategories';
 
 function App() {
   return (
   <>
-    <div className={styles.header}>
-      <Container>  
-        <Row>
-          <Col>
-            <NavbarCustom />
-          </Col>
-        </Row>
-      </Container>
-    </div>
+    <NavbarCustom />
     <SearchSection title="DeltaSets" searchBtnText="Filter" topCategoriesData={topCategories} />
   <div className={styles.carouselSection}>
      <Container>
